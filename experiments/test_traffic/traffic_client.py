623c54544d4ebfd6888546fd2638e978135117ca
RafaelDBeltran <rafaelbeltran.aluno@unipampa.edu.br>
2020-08-05 10:22:24 -0300

    ndn test update

--- experiments/test_traffic/traffic_client.py
+++ experiments/test_traffic/traffic_client.py
@@ -4,7 +4,7 @@ import sys
 
 sys.stdout = open("ndn_traffic_output.txt", "w")
 
-subprocess.run(["nfd-start"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.run(["nfd-start","-c","NFD_low_CS.conf"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 time.sleep(10)
 subprocess.run(["nfdc", "face", "create", "udp://"+sys.argv[1]])
 
@@ -13,7 +13,9 @@ subprocess.run(["nfdc", "route", "add", "/example/", "udp://"+sys.argv[1]])
 
 time.sleep(10)
 
-traffic_output = subprocess.run(["ndn-traffic-client", "-c", "2048", "-i", "1000", "ndn-traffic-client.conf"], stdout=subprocess.PIPE)
+#traffic_output = subprocess.run(["ndn-traffic-client", "-c", "2048", "-i", "1000", "ndn-traffic-client.conf"], stdout=subprocess.PIPE)
+
+traffic_output = subprocess.run(["ndn-traffic-client", "-c", "20", "ndn-traffic-client.conf"], stdout=subprocess.PIPE)
 print(traffic_output.stdout)
 
 subprocess.run(["nfd-stop"])
