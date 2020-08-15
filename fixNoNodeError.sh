0363e49f04c7b72597916696af01de47579b2a04
Rodrigo Mansilha <rodrigo.mansilha@inf.ufrgs.br>
2020-08-15 12:07:17 -0300

    clean

--- fixNoNodeError.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-pkill -9 -f zookeeper
-
-rm -r /home/rafael/Documents/ExtendEasyExp/~
-
-rm *.out
-
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-
-python controller_manager2.py start
-
-sleep 2
-
-python 30_trackerlens.py reset
