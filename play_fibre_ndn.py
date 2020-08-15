7acff3b4e99e50cbe92757cc873013cd7551e73a
Rodrigo Mansilha <rodrigo.mansilha@inf.ufrgs.br>
2020-08-15 16:23:08 -0300

    Update play_fibre_ndn.py

--- play_fibre_ndn.py
+++ play_fibre_ndn.py
@@ -15,6 +15,8 @@ import subprocess
 from functools import partial
 import shlex
 import zookeeper_controller as zc
+from kazoo.client import *
+import kazoo
 
 # constants
 ICN_STAGE_CMD = ['python3', 'icn-stage.py']
@@ -37,7 +39,7 @@ FAIL_ACTORS_MODELS += [[False, 2]] # without fail, with recover (avaliable)
 FAIL_ACTORS_MODELS += [[True, 1]] # with fail, without recover
 FAIL_ACTORS_MODELS += [[True, 2]] # with fail, with recover
 
-
+MAX_ATTEMPTS = 100
 MANAGE_DIRECTOR = True
 results = []
 
@@ -55,42 +57,46 @@ def get_file_name(fail, actors, experiment_length_secs=EXPERIMENT_LENGTH_SECS):
 	logging.info(" name: {}".format(name))
 	return name
 
+#
+# def define_iptables(jump):
+#
+# 	cmd = "cat busyactor.txt"
+# 	#busy_actor = commands.getoutput(cmd)
+# 	busy_actor = subprocess.check_output(cmd, shell = True)
+# 	busy_actor = busy_actor.decode('utf-8')
+# 	logging.info("[CP10.22] commands.getoutput: {}".format(busy_actor))
+#
+# 	cmd = "sudo iptables -{} INPUT -s {} -j DROP".format(jump, busy_actor)
+# 	logging.debug("CMD: {}".format(cmd))
+# 	subprocess.call(cmd, shell = True)
+# 	#subprocess.call("sudo iptables save", shell = True)
+# 	#subprocess.call("sudo iptables-save", shell = True)
 
-def define_iptables(jump):
 
-	cmd = "cat busyactor.txt"
-	#busy_actor = commands.getoutput(cmd)
-	busy_actor = subprocess.check_output(cmd, shell = True)
-	busy_actor = busy_actor.decode('utf-8')
-	logging.info("[CP10.22] commands.getoutput: {}".format(busy_actor))
-	
+def get_busy_actor(zk_addr='10.0.2.15:2181'):
 
-	cmd = "sudo iptables -{} INPUT -s {} -j DROP".format(jump, busy_actor)
-	logging.debug("CMD: {}".format(cmd))
-	subprocess.call(cmd, shell = True)
-	#subprocess.call("sudo iptables save", shell = True)
-	#subprocess.call("sudo iptables-save", shell = True)
+	logging.info("get_source zk_addr: {}".format(zk_addr))
+	zk = KazooClient(zk_addr, connection_retry=kazoo.retry.KazooRetry(max_tries=-1, max_delay=250))
+	logging.info("zk: {}".format(zk))
+	zk.add_listener(lambda x: os._exit(1) if x == KazooState.LOST else None)
+	zk.start()
+
+	count_attempts = 0
+	while count_attempts < MAX_ATTEMPTS:
+		count_attempts += 1
+		print("count_attempts: {}".format(count_attempts))
+		for actor in zk.get_children('/connected/busy_workers'):
+			print("found_actor!: {}".format(actor))
+			return actor
+		sleep(1)
+
+	return None
 
 
 def introduce_fault():
-	busy_actor = None  # "10.0.0.1"
+
 	start_time = time.time()
-	
-	#cmd = "python3 zookeeper_controller.py"
-
-	zc.get_source('10.200.0.6:2181')
-	
-	#logging.info("[CP10.21] commands.getoutput: {}".format(cmd))
-	#output = subprocess.check_output(cmd, shell=True)
-	#output = output.decode('utf-8')
-
-	cmd = "cat busyactor.txt"
-	logging.info("[CP10.22] commands.getoutput: {}".format(cmd))
-	busy_actor = subprocess.check_output(cmd, shell=True)
-	busy_actor = busy_actor.decode('utf-8')
-	#busy_actor = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=SLEEP_SECS_TO_FAIL)
-	logging.info("[CP10.3] busy_actor: '{}'".format(busy_actor))
-
+	busy_actor = get_busy_actor('10.200.0.6:2181')
 
 	diff_time = time.time() - start_time
 	logging.info("\n\n+--- FAIL EVALUATION [CP10.4] diff_time: {}\n".format(diff_time))
@@ -98,13 +104,15 @@ def introduce_fault():
 		sleep_secs = SLEEP_SECS_TO_FAIL - int(diff_time)
 		logging.info("\n\n+--- Sleeping {} secs ...  [CP10.5]".format(sleep_secs))
 		sleep(sleep_secs)
-		
-		#TODO 14/08 bloquear port ndn 6363 e zookeeper #fazer metodo
-		define_iptables('A')
-
 		logging.info("+--- introducing fail  [CP10.6]---+ busy actor: {} \n".format(busy_actor))
 
-		logging.info("+--- Finish fail ...  [CP10.7]---+\n")
+		cmd = "sudo iptables -A INPUT -s {} -j DROP".format(busy_actor)
+		logging.debug("Running cmd: {}".format(cmd))
+		cmd_shlex = shlex.split(cmd)
+		logging.debug("Running cmd_shlex: {}".format(cmd_shlex))
+		subprocess.call(cmd_shlex)
+
+		logging.debug("+--- Finish fail ...  [CP10.7]---+\n")
 	else:
 		raise Exception(" Source not found! ")
 
@@ -157,8 +165,12 @@ def run_play(fail_actors):
 		if fault_thread is not None:
 			logging.info("waiting for join fail thread... [CP10.5] timeout=---+\n")
 			fault_thread.join()
-		#TODO restaurar portas
-		define_iptables('D')
+
+		cmd = "sudo iptables -F"
+		logging.debug("Running cmd: {}".format(cmd))
+		cmd_shlex = shlex.split(cmd)
+		logging.debug("Running cmd_shlex: {}".format(cmd_shlex))
+		subprocess.call(cmd_shlex)
 
 		logging.info("")
 		logging.info("")
