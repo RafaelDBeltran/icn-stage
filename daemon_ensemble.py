cf284e5f8a5e8fce3845e8a3f39e977a2354d938
RafaelDBeltran <rafaelbeltran.aluno@unipampa.edu.br>
2021-11-28 15:25:34 -0300

    Teste

--- daemon_ensemble.py
+++ daemon_ensemble.py
@@ -123,10 +123,10 @@ class DirectorEnsembleDaemon(Daemon):
             if (self.role == 'leader') and (out != b''):
                 print('Sou o leader, sacou malandragem')
             elif (self.role == 'leader') and (out == b''):
-                subprocess.call("python3 /home/minion/icn-stage/icn-stage.py", shell=True)
+                #subprocess.call("python3 /home/minion/icn-stage/icn-stage.py", shell=True)
                 print('Sou o lider e iniciei o icn-stage')
             elif (self.role != 'leader') and (out != b''):
-                #subprocess.call("kill {} &".format(out.decode('utf-8')), shell=True)
+                subprocess.call("kill {} &".format(out.decode('utf-8')), shell=True)
                 print('Sou o seguidor e não sou mais continuo')
             else:
                 print('Sou o seguidor')
