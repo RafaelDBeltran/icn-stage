e63fbae97b4c11182dcee1637fb070d99d7851b0
RafaelDBeltran <rafaelbeltran.aluno@unipampa.edu.br>
2022-02-01 23:08:02 -0300

    Ajuste no experimento

--- experiments/test_traffic/traffic_server.py
+++ experiments/test_traffic/traffic_server.py
@@ -9,14 +9,14 @@ class NDN_traffic:
     def traffic_start(self):
 
         subprocess.run(["sudo nfd-stop"], shell=True)
-        subprocess.run(["sudo cp /home/vagrant/low.conf /usr/local/etc/mini-ndn/"],shell = True )
-        subprocess.run(["sudo nfd -c /usr/local/etc/ndn/low.conf &> /dev/null &"],shell = True)
+        #subprocess.run(["sudo cp /home/vagrant/low.conf /usr/local/etc/mini-ndn/"],shell = True )
+        subprocess.run(["sudo nfd -c low.conf &> /dev/null &"],shell = True)
 
         time.sleep(2)
 
         print("Starting NDN Traffic Server")
         try:
-            subprocess.run(["sudo ndn-traffic-server /home/vagrant/ndn-traffic-server.conf"], shell=True)
+            subprocess.run(["sudo ndn-traffic-server ndn-traffic-server.conf"], shell=True)
             logging.info("Waiting... ")
 
         except:
