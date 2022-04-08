35fd76413edb34199766a386eb422a04f45ce0a1
Rodrigo Mansilha <rmansilha@gmail.com>
2022-04-08 09:22:38 -0300

    modelos

--- python_kube.py
+++ python_kube.py
@@ -33,8 +33,10 @@ for i in ret.items:
 config = '''{\"zookeeper_adapter\": \"eth0\",\n
 "Nodes": [\n
 '''
-
+print("Creating config files...")
+print("\tDirectors:")
 for key, value in Diretores.items():
+    print("\t\tkey: {} \t value: {}".format(key, value))
     if Diretores_count > 1:
         config += ','
     config +=  '{\"remote_id\": \"'+str(Diretores_count)+'\", \"remote_hostname\": \"'+value+'\",\"remote_username\": \"icn_user\",\"remote_password\": \"icn_user\",\"remote_pkey_path\": \"/icn/keys/id_rsa\",\"Function": \"'+key+'\"} \n'
@@ -43,10 +45,14 @@ config += '''\n],
 \"workers\":[
 '''
 
+print("\tActors:")
 for key, value in Atores.items():
     if Atores_count > 1:
         config += ','
-    config +=  '{\"actor_id\": \"'+str(Atores_count)+'\", \"remote_hostname\": \"'+value+'\",\"remote_username\": \"icn_user\",\"remote_password\": \"icn_user\",\"remote_pkey_path\": \"/icn/keys/id_rsa\",\"Function": \"'+key+'\"} \n'
+    print("\t\tkey: {} \t value: {}".format(key, value))
+    config +=  '{\"actor_id\": \"'+str(Atores_count)+\
+               '\", \"remote_hostname\": \"'+value+\
+               '\",\"remote_username\": \"icn_user\",\"remote_password\": \"icn_user\",\"remote_pkey_path\": \"/icn/keys/id_rsa\",\"Function": \"'+key+'\"} \n'
     Atores_count += 1
 config += '''\n]'''
 
@@ -56,12 +62,20 @@ text_file = open("config.json", "w")
 text_file.write(config)
 text_file.close()
 
+print("\n\n\n")
+print("Setting config...")
+print("\tDirectors:")
 for key, _ in Diretores.items():
+    print("\t\tkey: {} \t value: {}".format(key, _))
     subprocess.run('kubectl cp config.json {}:/icn/icn-stage'.format(key),shell=True)
     subprocess.run('kubectl exec -it {} -- /bin/bash -c "sudo /etc/init.d/ssh start"'.format(key),shell=True)
 
+print("\tActors:")
 for key, _ in Atores.items():
+    print("\t\tkey: {} \t value: {}".format(key, _))
     subprocess.run('kubectl exec -it {} -- /bin/bash -c "sudo /etc/init.d/ssh start"'.format(key),shell=True)
 
-subprocess.run('kubectl cp config.json controlador:/icn/playground',shell=True)
-subprocess.run('cp config.json playground',shell=True)
+
+# print("\tController:")
+# subprocess.run('kubectl cp config.json controlador:/icn/playground',shell=True)
+# subprocess.run('cp config.json playground',shell=True)
